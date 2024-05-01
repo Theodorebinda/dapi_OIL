1a33bc7af1fca959a1f72103b418815b822031df
Theodoore samba <theodorebinda@gmail.com>
2024-05-01 10:30:00 +0100

    Commit

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,12 @@
-import { MetadataRoute } from "next";
+// import { MetadataRoute } from "next";
 
-const baseUrl = "https://dapioil.com";
+// const baseUrl = "https://dapioil.com";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = ["/", "/about", "/activites", "/rse", "/contact"];
-  return routes.map((route) => ({
-    url: `${baseUrl}${route}`,
-    changeFrequency: "weekly",
-    priority: route === "/" ? 1 : 0.7,
-  }));
-}
+// export default function sitemap(): MetadataRoute.Sitemap {
+//   const routes = ["/", "/about", "/activites", "/rse", "/contact"];
+//   return routes.map((route) => ({
+//     url: `${baseUrl}${route}`,
+//     changeFrequency: "weekly",
+//     priority: route === "/" ? 1 : 0.7,
+//   }));
+// }
